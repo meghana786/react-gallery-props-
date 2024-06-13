00441dc91a62ff690b5e692db7ad5d24eac51066
meghana786 <160600002+meghana786@users.noreply.github.com>
2024-06-13 10:37:42 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
-import DataComponent from './components/DataComponent'; 
-function App() {
+import DataComponent from './components/DataComponent';
+
+function App({ data }) {
   return (
     <div className="App">
       <h1>Image Gallery</h1>
-      <DataComponent /> 
+      <div className="image-container">
+        {data.map(item => (
+          <img key={item.id} src={item.img} alt={`Elephant ${item.id}`} />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+const AppWithData = () => (
+  <DataComponent>
+    <App />
+  </DataComponent>
+);
+
+export default AppWithData;
