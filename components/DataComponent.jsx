a4ebd85f71b3155ffbeb1a15c766e2b937e4078d
meghana786 <160600002+meghana786@users.noreply.github.com>
2024-06-13 10:38:33 +0530

    Update DataComponent.jsx

--- components/DataComponent.jsx
+++ components/DataComponent.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import elephant from '../images/elephant.jpeg'; 
+import elephant from '../images/elephant.jpeg';
 
-const DataComponent = () => {
-  const data = [
-    { id: 1, img: elephant },
-    { id: 2, img: elephant },
-    { id: 3, img: elephant },
-    { id: 4, img: elephant }
-  ];
+const data = [
+  { id: 1, img: elephant },
+  { id: 2, img: elephant },
+  { id: 3, img: elephant },
+  { id: 4, img: elephant }
+];
 
+const DataComponent = ({ children }) => {
   return (
-    <div className="image-container">
-      {data.map(item => (
-        <img key={item.id} src={item.img} alt={`Elephant ${item.id}`} />
-      ))}
+    <div>
+      {React.cloneElement(children, { data })}
     </div>
   );
 };
